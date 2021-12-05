a8b5191870616a4ecb4f0b9330df02981a09a9c8
huongcucnguyen <huongcucnguyen0889@gmail.com>
2021-12-05 16:08:58 +0700

    Update js file

--- js/myscript.js
+++ js/myscript.js
@@ -9,7 +9,7 @@ var yearBorn = prompt("Enter the year you were born");
 var age;
 age = calculateAge(yearBorn);
 
-console.log(name + " is " + age + "years old");
+console.log(name + " is " + age + " years old");
 
 var message = "";
 if (age < 18) {
@@ -29,6 +29,33 @@ if (age < 18) {
 }   else {
    message ="You are over 50 years old";
 
+}
+
+console.log(message);
+
+var gender = prompt("Are you male or female? (male/female)");
+switch (gender) {
+    case "male":
+        console.log("You are a man");
+        break;
+    case "female":
+        console.log("You are a woman");
+        break;
+    default:
+        console.log("Wrong gender input, undefine");
+}
+
+var isClose = confirm("Do you want to close current tab?");
+
+var message = "";
+console.log(isClose);
+
+if (isClose === true) {
+    for (var i=0; i<10; i++) {
+        message += " bye";
+    }
+} else {
+    message = "Thank you for staying with me";
 }
 
 console.log(message);
